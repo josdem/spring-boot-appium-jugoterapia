f1566f7883c2e9ed8f36b9b998d7d85357dc28f3
josdem <joseluis.delacruz@gmail.com>
2018-11-14 16:49:44 -0500

    Starting new feature testing project using Spring Boot and Appium :)

---
+++ src/main/java/com/jos/dem/springboot/appium/jugoterapia/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.jos.dem.springboot.appium.jugoterapia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppiumJugoterapiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppiumJugoterapiaApplication.class, args);
+	}
+
+}
