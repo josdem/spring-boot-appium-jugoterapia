16e55795087689e329f39916ba2b42ad71e88011
josdem <joseluis.delacruz@gmail.com>
2018-11-14 17:25:44 -0500

    #1 Adding Appium service

---
+++ src/main/java/com/jos/dem/springboot/appium/jugoterapia/service/AppiumService.java
@@ -0,0 +1,10 @@
+package com.jos.dem.springboot.appium.jugoterapia.service;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public interface AppiumService {
+
+  DesiredCapabilities getCapabilities();
+
+}
+
