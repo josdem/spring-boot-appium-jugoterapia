1dcfdf0bb27ef1f77cf6ea0f5c26b3b136aca4ad
josdem <joseluis.delacruz@gmail.com>
2018-11-14 17:50:35 -0500

    #1 Adding step definition

--- src/test/java/com/jos/dem/springboot/appium/jugoterapia/CategoryStep.java
+++ src/test/java/com/jos/dem/springboot/appium/jugoterapia/CategoryStep.java
@@ -1,3 +1,14 @@
 package com.jos.dem.springboot.appium.jugoterapia;
 
-public class CategoryStep extends BaseStep {}
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CategoryStep extends BaseStep {
+
+  @When("I launch the application")
+  public void shouldLaunchApplication() throws Exception {
+    log.info("Running: I launch the application at " + new Date().getTime());
+  }
+}
