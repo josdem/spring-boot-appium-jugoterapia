6dc88f6d42ec983e7bb8a5ab742467b83593b82f
josdem <joseluis.delacruz@gmail.com>
2018-11-14 17:44:09 -0500

    #1 Adding base step

---
+++ src/test/java/com/jos/dem/springboot/appium/jugoterapia/BaseStep.java
@@ -0,0 +1,8 @@
+package com.jos.dem.springboot.appium.jugoterapia;
+
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@ContextConfiguration(classes = AppiumJugoterapiaApplication.class)
+@WebAppConfiguration
+public class BaseStep {}
