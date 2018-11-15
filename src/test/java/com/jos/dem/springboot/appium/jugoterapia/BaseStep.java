880a866d254f365b7c1d65fb2b58c80029d7f15f
josdem <joseluis.delacruz@gmail.com>
2018-11-14 22:02:04 -0500

    #1 First test passed

--- src/test/java/com/jos/dem/springboot/appium/jugoterapia/BaseStep.java
+++ src/test/java/com/jos/dem/springboot/appium/jugoterapia/BaseStep.java
@@ -1,8 +1,22 @@
 package com.jos.dem.springboot.appium.jugoterapia;
 
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import com.jos.dem.springboot.appium.jugoterapia.service.AppiumService;
+
 @ContextConfiguration(classes = AppiumJugoterapiaApplication.class)
 @WebAppConfiguration
-public class BaseStep {}
+public class BaseStep {
+
+  @Autowired
+  private AppiumService appiumService;
+
+  DesiredCapabilities getCapabilities(){
+    return appiumService.getCapabilities();
+  }
+
+}
