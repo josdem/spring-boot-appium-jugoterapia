c4d0d2629efc43ced63dc65bb1a0b6a342d8d23a
josdem <joseluis.delacruz@gmail.com>
2018-11-15 13:37:01 -0500

    #3 Reading values from properties file

--- src/test/java/com/jos/dem/springboot/appium/jugoterapia/BaseStep.java
+++ src/test/java/com/jos/dem/springboot/appium/jugoterapia/BaseStep.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -21,14 +22,19 @@ import com.jos.dem.springboot.appium.jugoterapia.service.AppiumService;
 @WebAppConfiguration
 public class BaseStep {
 
+  @Value("${appium.server}")
+  private String appiumServer;
+  @Value("${appium.wait}")
+  private Long appiumWait;
+
   @Autowired
   private AppiumService appiumService;
   private AndroidDriver<AndroidElement> driver;
 
   public AndroidDriver<AndroidElement> getDriver() throws MalformedURLException {
     if(driver == null){
-      driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), appiumService.getCapabilities());
-      driver.manage().timeouts().implicitlyWait(new Long(10), TimeUnit.SECONDS);
+      driver = new AndroidDriver(new URL(appiumServer), appiumService.getCapabilities());
+      driver.manage().timeouts().implicitlyWait(appiumWait, TimeUnit.SECONDS);
     }
     return driver;
   }
